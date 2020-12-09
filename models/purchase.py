6dddc7a059d572dd2e2f2d4829ba8c4d81a22e8e
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-09 21:04:09 +0700

    Update purchase.py

--- models/purchase.py
+++ models/purchase.py
@@ -5,7 +5,7 @@ from odoo import models, fields
 class PurchaseOrderLine(models.Model):
 	_inherit = "purchase.order.line"
 
-	product_qty2 = fields.Float(string='Quantity 2', digits='Product Unit of Measure')
-	product_uom2 = fields.Many2one('uom.uom', string='Unit of Measure 2')
-	product_qty3 = fields.Float(string='Quantity 3', digits='Product Unit of Measure')
-	product_uom3 = fields.Many2one('uom.uom', string='Unit of Measure 3')
+	product_qty2 = fields.Float(string='Qty 2', digits='Product Unit of Measure')
+	product_uom2 = fields.Many2one('uom.uom', string='UoM 2')
+	product_qty3 = fields.Float(string='Qty 3', digits='Product Unit of Measure')
+	product_uom3 = fields.Many2one('uom.uom', string='UoM 3')
