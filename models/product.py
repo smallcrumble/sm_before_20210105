540ca171f88950f962898e86aca63867f038ca11
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-09 21:36:38 +0700

    Update product.py

--- models/product.py
+++ models/product.py
@@ -6,7 +6,7 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
 	_inherit = "product.template"
 	
-	uom_id2 = fields.Many2one('uom.uom', 'Unit of Measure 2', help="Extra unit of measure.")
-	uom_id3 = fields.Many2one('uom.uom', 'Unit of Measure 3', help="Extra unit of measure.")
+	uom_id2 = fields.Many2one('uom.uom', 'UoM 2', help="Extra unit of measure.")
+	uom_id3 = fields.Many2one('uom.uom', 'UoM 3', help="Extra unit of measure.")
 	qty2 = fields.Float()
 	qty3 = fields.Float()
