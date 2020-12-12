5d35dbcbfe6b4d91aba98a984d4ae29c2315f1bf
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-12 17:42:03 +0700

    update qty uom product purchase stock

--- models/purchase.py
+++ models/purchase.py
@@ -5,7 +5,7 @@ from odoo import models, fields
 class PurchaseOrderLine(models.Model):
 	_inherit = "purchase.order.line"
 
-	product_qty2 = fields.Float(string='Qty 2', digits='Product Unit of Measure')
-	product_uom2 = fields.Many2one('uom.uom', string='UoM 2')
-	product_qty3 = fields.Float(string='Qty 3', digits='Product Unit of Measure')
-	product_uom3 = fields.Many2one('uom.uom', string='UoM 3')
+	qty1 = fields.Float(string='Qty 1', digits='Product Unit of Measure')
+	uom1 = fields.Many2one('uom.uom', string='UoM 1')
+	qty2 = fields.Float(string='Qty 2', digits='Product Unit of Measure')
+	uom2 = fields.Many2one('uom.uom', string='UoM 2')
