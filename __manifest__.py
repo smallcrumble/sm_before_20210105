b3fc04ca63855b3b662bbaea5ab641fab36e0d0a
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-12 18:14:44 +0700

    update stock

--- __manifest__.py
+++ __manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/product_views.xml', 
         'views/purchase_views.xml',
-        #'views/stock_views.xml'
+        'views/stock_views.xml'
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
