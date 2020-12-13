b88c1b9d529aa309696d3f2a60d2cf399d7b9e57
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-13 20:38:22 +0700

    Update stock.py

--- models/stock.py
+++ models/stock.py
@@ -3,17 +3,6 @@
 from odoo import models, fields
 
 
-'''class Picking(models.Model):
-	_inherit = "stock.picking"
-	
-	qty1 = fields.Float('Demand 1')
-	done1 = fields.Float('Done 1')
-	uom1 = fields.Many2one('uom.uom', 'UoM 1', help="Extra unit of measure.")
-	qty2 = fields.Float('Demand 2')
-	done2 = fields.Float('Done 2')
-	uom2 = fields.Many2one('uom.uom', 'UoM 2', help="Extra unit of measure.")
-'''
-
 class StockMove(models.Model):
 	_inherit = "stock.move"
 	
@@ -48,3 +37,17 @@ class StockMoveLine(models.Model):
 		#res['uom2']=self.uom2.id
 		return res
 	'''
+
+class Picking(models.Model):
+    _inherit = "stock.picking"
+
+    def _check_immediate(self):
+		immediate_pickings = self.browse()
+		precision_digits = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+		for picking in self:
+			if all(float_is_zero(move_line.qty_done, precision_digits=precision_digits) and 
+				float_is_zero(move_line.qty_done1, precision_digits=precision_digits) and
+				float_is_zero(move_line.qty_done2, precision_digits=precision_digits) 
+				for move_line in picking.move_line_ids.filtered(lambda m: m.state not in ('done', 'cancel'))):
+					immediate_pickings |= picking
+		return immediate_pickings
