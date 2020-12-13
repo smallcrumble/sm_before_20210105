4f929688d85752115d59b6e8c0199f2a472f0225
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-13 20:57:11 +0700

    update purchase readonly uom

--- models/purchase.py
+++ models/purchase.py
@@ -17,3 +17,19 @@ class PurchaseOrderLine(models.Model):
 		res['qty2']=self.qty2
 		res['uom2']=self.uom2.id
 		return res
+
+	def _product_id_change(self):
+		if not self.product_id:
+			return
+
+		self.product_uom = self.product_id.uom_po_id or self.product_id.uom_id
+		self.uom1 = self.product_id.uom1
+		self.uom2 = self.product_id.uom2
+		product_lang = self.product_id.with_context(
+			lang=get_lang(self.env, self.partner_id.lang).code,
+			partner_id=self.partner_id.id,
+			company_id=self.company_id.id,
+		)
+		self.name = self._get_product_purchase_description(product_lang)
+
+		self._compute_tax_id()
