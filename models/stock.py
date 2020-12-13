383f399926d628dbb6506ec867092b5b23fb8967
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-13 15:35:10 +0700

    Update stock.py

--- models/stock.py
+++ models/stock.py
@@ -33,16 +33,16 @@ class StockMove(models.Model):
 		return res
 	
 class StockMoveLine(models.Model):
-    _inherit = "stock.move.line"
-   
+	_inherit = "stock.move.line"
+
 	qty_done1 = fields.Float('Done 1') #done1
 	uom1 = fields.Many2one('uom.uom', 'UoM 1', help="Extra unit of measure.")
 	qty_done2 = fields.Float('Done 2') #done2
 	uom2 = fields.Many2one('uom.uom', 'UoM 2', help="Extra unit of measure.")
 
-    '''def create_move(move_line):
-    	res = super(StockMoveLine, self).create_move(move_line)
-    	res['qty1']=self.qty_done1
+	'''def create_move(move_line):
+		res = super(StockMoveLine, self).create_move(move_line)
+		res['qty1']=self.qty_done1
 		#res['uom1']=self.uom1.id
 		res['qty2']=self.qty_done2
 		#res['uom2']=self.uom2.id
