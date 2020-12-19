05c7af699e3ae664904afdb5ea706dc8e6ae9891
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-19 21:36:18 +0700

    Update stock.py

--- models/stock.py
+++ models/stock.py
@@ -107,6 +107,29 @@ class StockMove(models.Model):
 					 for line_uom_id, qty in rec2.get(move.ids[0] if move.ids else move.id, [])
 				)
 
+	def _quantity_done_set(self):
+		quantity_done = self[0].quantity_done  # any call to create will invalidate `move.quantity_done`
+		quantity_done1 = self[0].done1
+		quantity_done2 = self[0].done2
+		for move in self:
+			move_lines = move._get_move_lines()
+			if not move_lines:
+				if quantity_done:
+					# do not impact reservation here
+					move_line = self.env['stock.move.line'].create(dict(move._prepare_move_line_vals(), qty_done=quantity_done))
+					move.write({'move_line_ids': [(4, move_line.id)]})
+			elif len(move_lines) == 1:
+				move_lines[0].qty_done = quantity_done
+				move_lines[0].qty_done1 = quantity_done1
+				move_lines[0].qty_done2 = quantity_done2
+			else:
+				# Bypass the error if we're trying to write the same value.
+				ml_quantity_done = 0
+				for move_line in move_lines:
+					ml_quantity_done += move_line.product_uom_id._compute_quantity(move_line.qty_done, move.product_uom, round=False)
+				if float_compare(quantity_done, ml_quantity_done, precision_rounding=move.product_uom.rounding) != 0:
+					raise UserError(_("Cannot set the done quantity from this stock move, work directly with the move lines."))
+
 class StockMoveLine(models.Model):
 	_inherit = "stock.move.line"
 
@@ -126,7 +149,152 @@ class StockMoveLine(models.Model):
 		return res
 	'''
 
-	
+	def write(self, vals):
+		if self.env.context.get('bypass_reservation_update'):
+			return super(StockMoveLine, self).write(vals)
+
+		if 'product_id' in vals and any(vals.get('state', ml.state) != 'draft' and vals['product_id'] != ml.product_id.id for ml in self):
+			raise UserError(_("Changing the product is only allowed in 'Draft' state."))
+
+		moves_to_recompute_state = self.env['stock.move']
+		Quant = self.env['stock.quant']
+		precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+		triggers = [
+			('location_id', 'stock.location'),
+			('location_dest_id', 'stock.location'),
+			('lot_id', 'stock.production.lot'),
+			('package_id', 'stock.quant.package'),
+			('result_package_id', 'stock.quant.package'),
+			('owner_id', 'res.partner')
+		]
+		updates = {}
+		for key, model in triggers:
+			if key in vals:
+				updates[key] = self.env[model].browse(vals[key])
+
+		# When we try to write on a reserved move line any fields from `triggers` or directly
+		# `product_uom_qty` (the actual reserved quantity), we need to make sure the associated
+		# quants are correctly updated in order to not make them out of sync (i.e. the sum of the
+		# move lines `product_uom_qty` should always be equal to the sum of `reserved_quantity` on
+		# the quants). If the new charateristics are not available on the quants, we chose to
+		# reserve the maximum possible.
+		if updates or 'product_uom_qty' in vals:
+			for ml in self.filtered(lambda ml: ml.state in ['partially_available', 'assigned'] and ml.product_id.type == 'product'):
+
+				if 'product_uom_qty' in vals:
+					new_product_uom_qty = ml.product_uom_id._compute_quantity(
+						vals['product_uom_qty'], ml.product_id.uom_id, rounding_method='HALF-UP')
+					# Make sure `product_uom_qty` is not negative.
+					if float_compare(new_product_uom_qty, 0, precision_rounding=ml.product_id.uom_id.rounding) < 0:
+						raise UserError(_('Reserving a negative quantity is not allowed.'))
+				else:
+					new_product_uom_qty = ml.product_qty
+
+				# Unreserve the old charateristics of the move line.
+				if not ml._should_bypass_reservation(ml.location_id):
+					try:
+						Quant._update_reserved_quantity(ml.product_id, ml.location_id, -ml.product_qty, lot_id=ml.lot_id, package_id=ml.package_id, owner_id=ml.owner_id, strict=True)
+					except UserError:
+						# If we were not able to unreserve on tracked quants, we can use untracked ones.
+						if ml.lot_id:
+							Quant._update_reserved_quantity(ml.product_id, ml.location_id, -ml.product_qty, lot_id=False, package_id=ml.package_id, owner_id=ml.owner_id, strict=True)
+						else:
+							raise
+
+				# Reserve the maximum available of the new charateristics of the move line.
+				if not ml._should_bypass_reservation(updates.get('location_id', ml.location_id)):
+					reserved_qty = 0
+					try:
+						q = Quant._update_reserved_quantity(ml.product_id, updates.get('location_id', ml.location_id), new_product_uom_qty, lot_id=updates.get('lot_id', ml.lot_id),
+															 package_id=updates.get('package_id', ml.package_id), owner_id=updates.get('owner_id', ml.owner_id), strict=True)
+						reserved_qty = sum([x[1] for x in q])
+					except UserError:
+						if updates.get('lot_id'):
+							# If we were not able to reserve on tracked quants, we can use untracked ones.
+							try:
+								q = Quant._update_reserved_quantity(ml.product_id, updates.get('location_id', ml.location_id), new_product_uom_qty, lot_id=False,
+																	 package_id=updates.get('package_id', ml.package_id), owner_id=updates.get('owner_id', ml.owner_id), strict=True)
+								reserved_qty = sum([x[1] for x in q])
+							except UserError:
+								pass
+					if reserved_qty != new_product_uom_qty:
+						new_product_uom_qty = ml.product_id.uom_id._compute_quantity(reserved_qty, ml.product_uom_id, rounding_method='HALF-UP')
+						moves_to_recompute_state |= ml.move_id
+						ml.with_context(bypass_reservation_update=True).product_uom_qty = new_product_uom_qty
+
+		# When editing a done move line, the reserved availability of a potential chained move is impacted. Take care of running again `_action_assign` on the concerned moves.
+		if updates or 'qty_done' in vals:
+			next_moves = self.env['stock.move']
+			mls = self.filtered(lambda ml: ml.move_id.state == 'done' and ml.product_id.type == 'product')
+			if not updates:  # we can skip those where qty_done is already good up to UoM rounding
+				mls = mls.filtered(lambda ml: not float_is_zero(ml.qty_done - vals['qty_done'], precision_rounding=ml.product_uom_id.rounding))
+			for ml in mls:
+				# undo the original move line
+				qty_done_orig = ml.move_id.product_uom._compute_quantity(ml.qty_done, ml.move_id.product_id.uom_id, rounding_method='HALF-UP')
+				in_date = Quant._update_available_quantity(ml.product_id, ml.location_dest_id, -qty_done_orig, lot_id=ml.lot_id,
+													  package_id=ml.result_package_id, owner_id=ml.owner_id)[1]
+				Quant._update_available_quantity(ml.product_id, ml.location_id, qty_done_orig, lot_id=ml.lot_id,
+													  package_id=ml.package_id, owner_id=ml.owner_id, in_date=in_date)
+
+				# move what's been actually done
+				product_id = ml.product_id
+				location_id = updates.get('location_id', ml.location_id)
+				location_dest_id = updates.get('location_dest_id', ml.location_dest_id)
+				qty_done = vals.get('qty_done', ml.qty_done)
+				qty_done1 = vals.get('qty_done1', ml.qty_done1)
+				qty_done2 = vals.get('qty_done2', ml.qty_done2)
+				lot_id = updates.get('lot_id', ml.lot_id)
+				package_id = updates.get('package_id', ml.package_id)
+				result_package_id = updates.get('result_package_id', ml.result_package_id)
+				owner_id = updates.get('owner_id', ml.owner_id)
+				quantity = ml.move_id.product_uom._compute_quantity(qty_done, ml.move_id.product_id.uom_id, rounding_method='HALF-UP')
+				if not ml._should_bypass_reservation(location_id):
+					ml._free_reservation(product_id, location_id, quantity, lot_id=lot_id, package_id=package_id, owner_id=owner_id)
+				if not float_is_zero(quantity, precision_digits=precision):
+					available_qty, in_date = Quant._update_available_quantity(product_id, location_id, -quantity, lot_id=lot_id, package_id=package_id, owner_id=owner_id)
+					if available_qty < 0 and lot_id:
+						# see if we can compensate the negative quants with some untracked quants
+						untracked_qty = Quant._get_available_quantity(product_id, location_id, lot_id=False, package_id=package_id, owner_id=owner_id, strict=True)
+						if untracked_qty:
+							taken_from_untracked_qty = min(untracked_qty, abs(available_qty))
+							Quant._update_available_quantity(product_id, location_id, -taken_from_untracked_qty, lot_id=False, package_id=package_id, owner_id=owner_id)
+							Quant._update_available_quantity(product_id, location_id, taken_from_untracked_qty, lot_id=lot_id, package_id=package_id, owner_id=owner_id)
+							if not ml._should_bypass_reservation(location_id):
+								ml._free_reservation(ml.product_id, location_id, untracked_qty, lot_id=False, package_id=package_id, owner_id=owner_id)
+					Quant._update_available_quantity(product_id, location_dest_id, quantity, lot_id=lot_id, package_id=result_package_id, owner_id=owner_id, in_date=in_date)
+
+				# Unreserve and reserve following move in order to have the real reserved quantity on move_line.
+				next_moves |= ml.move_id.move_dest_ids.filtered(lambda move: move.state not in ('done', 'cancel'))
+
+				# Log a note
+				if ml.picking_id:
+					ml._log_message(ml.picking_id, ml, 'stock.track_move_template', vals)
+
+		res = super(StockMoveLine, self).write(vals)
+
+		# Update scrap object linked to move_lines to the new quantity.
+		if 'qty_done' in vals:
+			for move in self.mapped('move_id'):
+				if move.scrapped:
+					move.scrap_ids.write({'scrap_qty': move.quantity_done})
+
+		# As stock_account values according to a move's `product_uom_qty`, we consider that any
+		# done stock move should have its `quantity_done` equals to its `product_uom_qty`, and
+		# this is what move's `action_done` will do. So, we replicate the behavior here.
+		if updates or 'qty_done' in vals:
+			moves = self.filtered(lambda ml: ml.move_id.state == 'done').mapped('move_id')
+			moves |= self.filtered(lambda ml: ml.move_id.state not in ('done', 'cancel') and ml.move_id.picking_id.immediate_transfer and not ml.product_uom_qty).mapped('move_id')
+			for move in moves:
+				move.product_uom_qty = move.quantity_done
+				move.qty1 = move.done1
+				move.qty2 = move.done2
+			next_moves._do_unreserve()
+			next_moves._action_assign()
+
+		if moves_to_recompute_state:
+			moves_to_recompute_state._recompute_state()
+
+		return res
 
 class StockImmediateTransfer(models.TransientModel):
 	_inherit = 'stock.immediate.transfer'
