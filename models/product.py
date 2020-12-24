210f52649296184a675c36ba9e120a9dfc231acf
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-24 22:33:55 +0700

    product

--- models/product.py
+++ models/product.py
@@ -9,10 +9,10 @@ class ProductTemplate(models.Model):
 	uom_id1 = fields.Many2one('uom.uom', 'UoM 1', help="Extra unit of measure.")
 	uom_id2 = fields.Many2one('uom.uom', 'UoM 2', help="Extra unit of measure.")
 	qty_available1 = fields.Float(
-		'Quantity On Hand', compute='_compute_quantities', search='_search_qty_available',
+		'Quantity On Hand 1', compute='_compute_quantities', search='_search_qty_available',
 		compute_sudo=False, digits='Product Unit of Measure')
 	qty_available2 = fields.Float(
-		'Quantity On Hand', compute='_compute_quantities', search='_search_qty_available',
+		'Quantity On Hand 2', compute='_compute_quantities', search='_search_qty_available',
 		compute_sudo=False, digits='Product Unit of Measure')
 	#qty1 = fields.Float('Qty 1')
 	#qty2 = fields.Float('Qty 2')
