3a11faa0a706b89e2d07d7fe729f8af928b2ab11
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-24 22:10:34 +0700

    Update product.py

--- models/product.py
+++ models/product.py
@@ -6,7 +6,16 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
 	_inherit = "product.template"
 	
-	uom1 = fields.Many2one('uom.uom', 'UoM 1', help="Extra unit of measure.")
-	uom2 = fields.Many2one('uom.uom', 'UoM 2', help="Extra unit of measure.")
-	qty1 = fields.Float('Qty 1')
-	qty2 = fields.Float('Qty 2')
+	uom_id1 = fields.Many2one('uom.uom', 'UoM 1', help="Extra unit of measure.")
+	uom_id2 = fields.Many2one('uom.uom', 'UoM 2', help="Extra unit of measure.")
+	qty_available1 = fields.Float(
+		'Quantity On Hand', compute='_compute_quantities', search='_search_qty_available',
+		compute_sudo=False, digits='Product Unit of Measure')
+	qty_available2 = fields.Float(
+		'Quantity On Hand', compute='_compute_quantities', search='_search_qty_available',
+		compute_sudo=False, digits='Product Unit of Measure')
+	#qty1 = fields.Float('Qty 1')
+	#qty2 = fields.Float('Qty 2')
+	_defaults = {
+		'type' : 'product'
+	}
