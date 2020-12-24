1108790d4ab4242545c39d163182303ad54829dc
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2020-12-24 23:39:24 +0700

    Update purchase.py

--- models/purchase.py
+++ models/purchase.py
@@ -6,17 +6,21 @@ from odoo.tools.misc import formatLang, get_lang
 class PurchaseOrderLine(models.Model):
 	_inherit = "purchase.order.line"
 
-	qty1 = fields.Float(string='Qty 1', digits='Product Unit of Measure')
-	uom1 = fields.Many2one('uom.uom', string='UoM 1')
-	qty2 = fields.Float(string='Qty 2', digits='Product Unit of Measure')
-	uom2 = fields.Many2one('uom.uom', string='UoM 2')
+	#qty1 = fields.Float(string='Qty 1', digits='Product Unit of Measure')
+	product_qty1 = fields.Float(string='Quantity 1', digits='Product Unit of Measure')	
+	#qty2 = fields.Float(string='Qty 2', digits='Product Unit of Measure')
+	product_qty2 = fields.Float(string='Quantity 2', digits='Product Unit of Measure')
+	#uom1 = fields.Many2one('uom.uom', string='UoM 1')
+	product_uom1 = fields.Many2one('uom.uom', string='Unit of Measure', domain="[('category_id', '=', product_uom_category_id)]")
+	#uom2 = fields.Many2one('uom.uom', string='UoM 2')
+	product_uom2 = fields.Many2one('uom.uom', string='Unit of Measure', domain="[('category_id', '=', product_uom_category_id)]")
 
 	def _prepare_stock_move_vals(self, picking, price_unit, product_uom_qty, product_uom):
 		res = super(PurchaseOrderLine, self)._prepare_stock_move_vals(picking, price_unit, product_uom_qty, product_uom)
-		res['qty1']=self.qty1
-		res['uom1']=self.uom1.id
-		res['qty2']=self.qty2
-		res['uom2']=self.uom2.id
+		res['product_uom_qty1']=self.product_qty1
+		res['product_uom1']=self.product_uom1.id
+		res['product_uom_qty2']=self.product_qty2
+		res['product_uom2']=self.product_uom2.id
 		return res
 
 	def _product_id_change(self):
@@ -24,8 +28,8 @@ class PurchaseOrderLine(models.Model):
 			return
 
 		self.product_uom = self.product_id.uom_po_id or self.product_id.uom_id
-		self.uom1 = self.product_id.uom1
-		self.uom2 = self.product_id.uom2
+		self.product_uom1 = self.product_id.uom_id1
+		self.product_uom2 = self.product_id.uom_id2
 		product_lang = self.product_id.with_context(
 			lang=get_lang(self.env, self.partner_id.lang).code,
 			partner_id=self.partner_id.id,
